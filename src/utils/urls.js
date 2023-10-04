36ae042a13746ad837b487aa262105fe0c48f7df
Rajkumar V B <rajkumarvb.career@gmail.com>
2023-10-04 22:08:36 +0530

    Update urls.js

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 export const TATKAL = "http://127.0.0.1:5000/tatkal";
-export const TATKAL_FOOD = "http://127.0.0.1:5000/tatkal";
+export const TATKAL_FOOD = "http://127.0.0.1:5000/tatkal_food";
 export const CREDIT_CARD = "http://127.0.0.1:5000/tirupati/credit_card";
 export const SEVA = "http://127.0.0.1:5000/tirupati/seva";
 export const ACCOMODATION = "http://127.0.0.1:5000/tirupati/accomodation";
